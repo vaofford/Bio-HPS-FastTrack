7235de6cfe18de78727f8cf3f7fa324b3f39098e
js21 <js21@sanger.ac.uk>
2015-03-24 01:42:25 +0000

    Changed command line argument form analysis to pipeline

--- lib/Bio/HPS/FastTrack/CommandLine/HPSFastTrack.pm
+++ lib/Bio/HPS/FastTrack/CommandLine/HPSFastTrack.pm
@@ -17,27 +17,27 @@ has 'help'        => ( is => 'rw', isa => 'Bool',     default  => 0 );
 
 has 'study' => ( is => 'rw', isa => 'Str');
 has 'database'   => ( is => 'rw', isa => 'Str');
-has 'analysis'   => ( is => 'rw', isa => 'Maybe[ArrayRef]', default => sub { ['all'] } );
+has 'pipeline'   => ( is => 'rw', isa => 'Maybe[ArrayRef]', default => sub { ['all'] } );
 has 'mode'   => ( is => 'rw', isa => 'Str', default => '' );
 
 sub BUILD {
 
     my ($self) = @_;
 
-    my ( $study, $database, $analysis, $mode, $help );
+    my ( $study, $database, $pipeline, $mode, $help );
 
     GetOptionsFromArray(
 			$self->args,
 			's|study=s' => \$study,
 			'd|db=s'   => \$database,
-			'a|analysis=s@' =>\$analysis,
+			'p|pipeline=s@' =>\$pipeline,
 			'm|mode:s' =>\$mode,
 			'h|help'           => \$help,
     );
 
     $self->study($study) if ( defined($study) );
     $self->database($database)     if ( defined($database) );
-    $self->analysis($analysis)     if ( defined($analysis) );
+    $self->pipeline($pipeline)     if ( defined($pipeline) );
     $self->mode($mode) if ( defined($mode) );
 
 }
@@ -45,12 +45,12 @@ sub BUILD {
 sub run {
     my ($self) = @_;
 
-    ( $self->study && $self->database && $self->analysis ) or die <<USAGE;
+    ( $self->study && $self->database && $self->pipeline ) or die <<USAGE;
 	
 Usage:
   -s|study         <>
   -d|database      <>
-  -a|analysis      <> 
+  -a|pipeline      <> 
   -h|help          <print this message>
 
 Utility script to fast track high priority samples through the Pathogen Informatics pipelines
@@ -61,7 +61,7 @@ USAGE
     my $hps_fast_track = Bio::HPS::FastTrack->new(
 				      study => $self->study,
 				      database   => $self->database,
-				      analysis => $self->analysis,
+				      pipeline => $self->pipeline,
 				      mode => $self->mode,
 				     );
 
